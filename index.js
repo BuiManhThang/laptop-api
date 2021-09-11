a75cdeda99e3052edf195aa75d0014368ad18a64
Arthuria Ex <arduinomk2016@gmail.com>
2021-09-11 21:50:39 +0700

    add port

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => {
